0b7b0b652a4babfdff2600e79b18f82e6ef1a1b0
Wenderson Gustavo Santos <guto15santos@gmail.com>
2023-07-26 16:04:55 -0300

    Escutando eventos: event.preventDefault()

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -15,14 +15,19 @@ const Formulario = () => {
         'Inovação e Gestão' 
     ]
 
+    const submissao = (evento) => {
+        evento.preventDefault()
+       console.log("Foi Submetido")
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={submissao}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label='Nome' placeholder='Digite seu nome' />
-                <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
+                <CampoTexto obrigatorio={true} label='Nome' placeholder='Digite seu nome' />
+                <CampoTexto obrigatorio={true} label='Cargo' placeholder='Digite seu cargo' />
                 <CampoTexto label='Imagem' placeholder='Informe o endereço da imagem' />
-                <ListaSuspensa itens={times}/>
+                <ListaSuspensa obrigatorio={true} itens={times}/>
                 <Botao>
                     Criar Card
                 </Botao>
