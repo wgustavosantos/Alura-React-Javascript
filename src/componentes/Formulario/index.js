54e5f25ad72611a1a2487c9d1ee5be69bb94c665
Wenderson Gustavo Santos <guto15santos@gmail.com>
2023-07-26 15:50:20 -0300

    Criando o botão: props.children

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,7 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto/';
 import ListaSuspensa from '../ListaSuspensa/ListaSuspensa';
+import Botao from '../Botao';
 
 const Formulario = () => {
 
@@ -22,6 +23,9 @@ const Formulario = () => {
                 <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
                 <CampoTexto label='Imagem' placeholder='Informe o endereço da imagem' />
                 <ListaSuspensa itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
