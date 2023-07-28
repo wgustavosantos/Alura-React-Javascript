a217a06754e9e3725ab6a1dae79077edf3be1d93
Wenderson Gustavo Santos <guto15santos@gmail.com>
2023-07-28 15:35:58 -0300

    Card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/wgustavosantos.png' alt='' />
+            </div>
+
+            <div className='rodape'>
+                <h4>Gustavo</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
@@ -7,6 +8,7 @@ const Time = (props) => {
     return (
         <section className='time' style={cssSection}>
             <h3 style={cssH3}>{props.nome}</h3>
+            <Colaborador/>
         </section>
     )
 }
